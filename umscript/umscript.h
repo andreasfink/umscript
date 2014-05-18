579895214ac8bfcf7a221727a271df1180e67bdb
Andreas Fink <andreas@fink.org>
2014-05-19 01:18:27 +0200

    Initial Commit

---
+++ umscript/umscript.h
@@ -0,0 +1,13 @@
+//
+//  umscript.h
+//  umscript
+//
+//  Created by Andreas Fink on 19.05.14.
+//  Copyright (c) 2014 SMSRelay AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface umscript : NSObject
+
+@end
---
+++ umscript/umscript.m
@@ -0,0 +1,13 @@
+//
+//  umscript.m
+//  umscript
+//
+//  Created by Andreas Fink on 19.05.14.
+//  Copyright (c) 2014 SMSRelay AG. All rights reserved.
+//
+
+#import "umscript.h"
+
+@implementation umscript
+
+@end
