5af2c36bdc60e451af111ab80a11545075a04c26
Andreas Fink <andreas@fink.org>
2014-06-11 20:05:23 +0200

    new makefiles

--- make_pkg.sh
+++ make_pkg.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
 PROJECT_NAME="$1"
-PKG_INSTALL_ROOT="$2"
-PKG_INSTALL_RESOURCES="$3"
-PKG_INSTALL_SCRIPTS="$4"
+PKG_INSTALL_ROOT=$2
+PKG_INSTALL_RESOURCES=$3
+PKG_INSTALL_SCRIPTS=$4
 PKG_IDENTIFIER="com.smsrelay.${PROJECT_NAME}"
-
 rm -rf "${PKG_INSTALL_ROOT}"
-mkdir -p "${PKG_INSTALL_ROOT}" "${PKG_INSTALL_RESOURCES}" "${PKG_INSTALL_SCRIPTS}"
+mkdir -p "${PKG_INSTALL_ROOT}" "${PKG_INSTALL_SCRIPTS}"
 
-make DESTDIR="${PKG_INSTALL_ROOT}" install_for_pkg
-
+xcodebuild DSTROOT="${PKG_INSTALL_ROOT}" install
 
 SIGNING_KEY_INSTALLER="Developer ID Installer: SMSRelay AG"
-SIGNING_KEY_LIB="Developer ID Application: SMSRelay AG (WMTUF4B7XX)"
-PKGPREFIX=
-
 VERSION=`cat VERSION`
-LONGVER="$VERSION build  on `date`"
-SHORTVER="$VERSION"
-OUTPUT_FILE="${PROJECT_NAME}_${VERSION}.pkg"
+BUILDDATE=`date +%Y%m%d%H%M`
+OUTPUT_FILE="${PROJECT_NAME}_${VERSION}_${BUILDDATE}.pkg"
 
 pkgbuild --root "${PKG_INSTALL_ROOT}" \
-	--install-location / \
-	--scripts "${PKG_INSTALL_SCRIPTS}" 						\
-	--sign "${SIGNING_KEY_INSTALLER}"  						\
-	--version "${LONGVER}" \
-	--identifier "${PKG_IDENTIFIER}" \
-	"${OUTPUT_FILE}"
+    --install-location / \
+    --scripts "${PKG_INSTALL_SCRIPTS}" 						\
+    --sign "${SIGNING_KEY_INSTALLER}"  						\
+    --version "${VERSION}" \
+    --identifier "${PKG_IDENTIFIER}" \
+    "${OUTPUT_FILE}"
