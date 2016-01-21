7fb092776953e2572a4fdcb59705a812ac54ca8c
Andreas Fink <andreas@fink.org>
2016-01-21 01:45:48 +0100

    minor cleanups

--- language/_generated_umscript.y.h
+++ language/_generated_umscript.y.h
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.7.  */
+/* A Bison parser, made by GNU Bison 3.0.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,13 +26,13 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
 #ifndef YY_YY_LANGUAGE_GENERATED_UMSCRIPT_Y_H_INCLUDED
 # define YY_YY_LANGUAGE_GENERATED_UMSCRIPT_Y_H_INCLUDED
-/* Enabling traces.  */
+/* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
 #endif
@@ -40,108 +40,93 @@
 extern int yydebug;
 #endif
 /* "%code requires" blocks.  */
-/* Line 2058 of yacc.c  */
-#line 11 "language/umscript.y"
+#line 11 "language/umscript.y" /* yacc.c:1915  */
 
     typedef void*                 yyscan_t;
     @class UMScriptCompilerEnvironment;
 
+#line 49 "language/_generated_umscript.y.h" /* yacc.c:1915  */
 
-/* Line 2058 of yacc.c  */
-#line 52 "language/_generated_umscript.y.h"
-
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     IDENTIFIER = 258,
-     VARIABLE = 259,
-     FIELD = 260,
-     CONST_BOOLEAN = 261,
-     CONST_STRING = 262,
-     CONST_HEX = 263,
-     CONST_LONGLONG = 264,
-     CONST_BINARY = 265,
-     CONST_INTEGER = 266,
-     CONST_OCTAL = 267,
-     CONST_DOUBLE = 268,
-     OPERATOR_ASSIGNMENT = 269,
-     OPERATOR_INCREASE = 270,
-     OPERATOR_DECREASE = 271,
-     OPERATOR_LEFT = 272,
-     OPERATOR_RIGHT = 273,
-     OPERATOR_LESS = 274,
-     OPERATOR_LESS_OR_EQUAL = 275,
-     OPERATOR_GREATER = 276,
-     OPERATOR_GREATER_OR_EQUAL = 277,
-     OPERATOR_EQUAL = 278,
-     OPERATOR_NOT_EQUAL = 279,
-     OPERATOR_AND = 280,
-     OPERATOR_OR = 281,
-     OPERATOR_MUL_ASSIGN = 282,
-     OPERATOR_DIV_ASSIGN = 283,
-     OPERATOR_MOD_ASSIGN = 284,
-     OPERATOR_ADD_ASSIGN = 285,
-     OPERATOR_SUB_ASSIGN = 286,
-     OPERATOR_LEFT_ASSIGN = 287,
-     OPERATOR_RIGHT_ASSIGN = 288,
-     OPERATOR_AND_ASSIGN = 289,
-     OPERATOR_XOR_ASSIGN = 290,
-     OPERATOR_OR_ASSIGN = 291,
-     CASE = 292,
-     DEFAULT = 293,
-     IF = 294,
-     ELSE = 295,
-     SWITCH = 296,
-     WHILE = 297,
-     DO = 298,
-     FOR = 299,
-     GOTO = 300,
-     CONTINUE = 301,
-     BREAK = 302,
-     RETURN = 303,
-     FUNC_NAME = 304,
-     FILE_NAME = 305,
-     LINE_NUMBER = 306
-   };
+  enum yytokentype
+  {
+    IDENTIFIER = 258,
+    VARIABLE = 259,
+    FIELD = 260,
+    CONST_BOOLEAN = 261,
+    CONST_STRING = 262,
+    CONST_HEX = 263,
+    CONST_LONGLONG = 264,
+    CONST_BINARY = 265,
+    CONST_INTEGER = 266,
+    CONST_OCTAL = 267,
+    CONST_DOUBLE = 268,
+    OPERATOR_ASSIGNMENT = 269,
+    OPERATOR_INCREASE = 270,
+    OPERATOR_DECREASE = 271,
+    OPERATOR_LEFT = 272,
+    OPERATOR_RIGHT = 273,
+    OPERATOR_LESS = 274,
+    OPERATOR_LESS_OR_EQUAL = 275,
+    OPERATOR_GREATER = 276,
+    OPERATOR_GREATER_OR_EQUAL = 277,
+    OPERATOR_EQUAL = 278,
+    OPERATOR_NOT_EQUAL = 279,
+    OPERATOR_AND = 280,
+    OPERATOR_OR = 281,
+    OPERATOR_MUL_ASSIGN = 282,
+    OPERATOR_DIV_ASSIGN = 283,
+    OPERATOR_MOD_ASSIGN = 284,
+    OPERATOR_ADD_ASSIGN = 285,
+    OPERATOR_SUB_ASSIGN = 286,
+    OPERATOR_LEFT_ASSIGN = 287,
+    OPERATOR_RIGHT_ASSIGN = 288,
+    OPERATOR_AND_ASSIGN = 289,
+    OPERATOR_XOR_ASSIGN = 290,
+    OPERATOR_OR_ASSIGN = 291,
+    CASE = 292,
+    DEFAULT = 293,
+    IF = 294,
+    ELSE = 295,
+    SWITCH = 296,
+    WHILE = 297,
+    DO = 298,
+    FOR = 299,
+    GOTO = 300,
+    CONTINUE = 301,
+    BREAK = 302,
+    RETURN = 303,
+    FUNC_NAME = 304,
+    FILE_NAME = 305,
+    LINE_NUMBER = 306
+  };
 #endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
 
-#ifdef YYPARSE_PARAM
-#if defined __STDC__ || defined __cplusplus
-int yyparse (void *YYPARSE_PARAM);
-#else
-int yyparse ();
-#endif
-#else /* ! YYPARSE_PARAM */
-#if defined __STDC__ || defined __cplusplus
+
 int yyparse (yyscan_t yyscanner, UMScriptCompilerEnvironment *cenv);
-#else
-int yyparse ();
-#endif
-#endif /* ! YYPARSE_PARAM */
 
 #endif /* !YY_YY_LANGUAGE_GENERATED_UMSCRIPT_Y_H_INCLUDED  */
