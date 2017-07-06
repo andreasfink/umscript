91f7a1b8f0f636962e6f95d32e32f196969a7e9a
Andreas Fink <andreas@fink.org>
2017-07-07 00:24:48 +0200

    moved the glue functions out a bit to compile separately for c

--- umscript-glue/UMFunction_CGlue.m
+++ umscript-glue/UMFunction_CGlue.m
@@ -54,7 +54,7 @@ CFDiscreteValueRef term_evaluate_with_environment(CFTermRef xterm, CFEnvironment
 
 CFTermRef term_get(CFArrayRef params,int pos)
 {
-    NSArray *a = (__bridge NSArray *)params;
+    NSArray *a = (NSArray *)params;
     if((pos <0) || (pos >= a.count))
     {
         return NULL;
