c13d145a64b0573456a18761c0a1f50f2c1160df
Andreas Fink <andreas@fink.org>
2018-08-02 14:49:04 +0200

    parse non Latin Unicode characters correctly...

--- umscript/NSString+UMScript.m
+++ umscript/NSString+UMScript.m
@@ -15,8 +15,9 @@
 
 - (UMDiscreteValue *)discreteValue
 {
-    const unsigned char *digits = (const unsigned char *)self.UTF8String;
-    size_t len = strlen((const char *)digits);
+
+    size_t len = [self length]; /* this might be a UTF8 string with foreign characters. So strlen wont work */
+
     if(len == 0)
     {
         return [UMDiscreteValue discreteNull];
@@ -35,6 +36,7 @@
     BOOL startsWithSingleQuote = NO;
     BOOL endsWithSingleQuote = NO;
     BOOL endsWithLL = NO;
+    NSString *digits;
 
     if([self isEqualToString:@"YES"] || [self isEqualToString:@"true"])
     {
@@ -48,9 +50,27 @@
     {
         return [UMDiscreteValue discreteNull];
     }
+    unichar previous='\0';
+    unichar c='\0';
+    unichar last='\0';
+    unichar preLast='\0';
+    unichar first='\0';
+    unichar second='\0';
+    if(len >= 1)
+    {
+        last = [self characterAtIndex:len-1];
+        first = [self characterAtIndex:0];
+    }
+    if(len >= 2)
+    {
+        preLast = [self characterAtIndex:len-2];
+        second = [self characterAtIndex:1];
+    }
+
     for(size_t i=0;i<len;i++)
     {
-        char c = digits[i];
+        previous = c;
+        c = [self characterAtIndex:i];
         if((i==0) && (c=='0') && (len > 1))
         {
             hasLeadingZero = YES;
@@ -75,7 +95,7 @@
         {
             endsWithSingleQuote = YES;
         }
-        if((i==(len-2)) && (c=='L') && (digits[len-1] == 'L'))
+        if((i==(len-2)) && (c=='L') && (previous == 'L'))
         {
             endsWithLL = YES;
             break; /* we dont want the LL to be processed */
@@ -102,37 +122,44 @@
         }
     }
 
-    if(startsWithDoubleQuote && endsWithDoubleQuote)
+    if((startsWithDoubleQuote && endsWithDoubleQuote) && (len>=2))
     {
-        /* FIXME: we should probably parse things like \t or \n here ? what about \" ? */
         NSString *s = [self substringWithRange:NSMakeRange(1,len-2)];
         return [UMDiscreteValue discreteString:s];
     }
     if(startsWithSingleQuote && endsWithSingleQuote)
     {
+        NSString *s = [self substringWithRange:NSMakeRange(1,len-2)];
+        return [UMDiscreteValue discreteString:s];
+/* what was this ?!
         int value = 0;
         for(size_t i=1;i<(len-1);i++)
         {
-            value = (value * 256) + (digits[i]);
+            unichar c = [self characterAtIndex:i];
+            value = (value * 256) + c;
         }
         return [UMDiscreteValue discreteInt:value];
+ */
     }
     if(endsWithLL)
     {
-        NSString *s = [self substringWithRange:NSMakeRange(0,len-2)];
-        digits = (const unsigned char *)s.UTF8String;
+        digits = [self substringWithRange:NSMakeRange(0,len-2)];
         len = len - 2;
+    }
+    else
+    {
+        digits = [self copy];
     }
 
     if((onlyDigits==YES) && (hasLeadingZero==NO) && (hasDot == NO))
     {
         if(endsWithLL)
         {
-            return [UMDiscreteValue discreteLongLong:atoll((const char *)digits)];
+            return [UMDiscreteValue discreteLongLong:atoll((const char *)digits.UTF8String)];
         }
         else
         {
-            return [UMDiscreteValue discreteInt:atoi((const char *)digits)];
+            return [UMDiscreteValue discreteInt:atoi((const char *)digits.UTF8String)];
         }
     }
     if((onlyOcto==YES) && (hasLeadingZero == YES) && (hasDot == NO))
@@ -140,7 +167,8 @@
         long long value = 0;
         for(size_t i=1;i<len;i++)
         {
-            value = (value * 8) + (digits[i]-'0');
+            unichar c = [self characterAtIndex:i];
+            value = (value * 8) + (c-'0');
         }
         if(endsWithLL)
         {
@@ -156,18 +184,18 @@
         long long value = 0;
         for(int i=2;i<len;i++)
         {
-            char c = digits[i];
+            unichar c = [self characterAtIndex:i];
             if((c >='0') && (c<='9'))
             {
-                value = (value * 16) + (digits[i]-'0');
+                value = (value * 16) + (c-'0');
             }
             else if((c >='a') && (c<='f'))
             {
-                value = (value * 16) + (digits[i]-'a'+10);
+                value = (value * 16) + (c-'a'+10);
             }
             else if((c >='A') && (c<='F'))
             {
-                value = (value * 16) + (digits[i]-'A'+10);
+                value = (value * 16) + (c-'A'+10);
             }
         }
         if(endsWithLL)
@@ -181,7 +209,7 @@
     }
     if(onlyDouble && hasDot)
     {
-        double d = atof((const char *)digits);
+        double d = atof((const char *)digits.UTF8String);
         return [UMDiscreteValue discreteDouble:d];
 
     }
